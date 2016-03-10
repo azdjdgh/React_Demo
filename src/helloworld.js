304a595c1c7b2ba8077a3f9b927fa17df9228c9d
azdjdgh <zhaixiaoqing_0202@163.com>
2016-03-10 15:29:31 +0800

    提交项目

---
+++ src/helloworld.js
@@ -0,0 +1,11 @@
+<script src="build/react.js"></script>
+<script src="build/JSXTransformer.js"></script>
+
+
+React.render(
+    <h1>hell0,111111111</h1>,
+    document.getElementById('example')
+    
+);
+
+  
